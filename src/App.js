1a82ca5c051b68fc6960d681cf21c914f47a55b6
Jaeho Choi <jaeho@Jaehoui-MacBookPro.local>
2020-06-02 11:47:42 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+const customers=[{
+  'id':1,
+  'image':'https://placeimg.com/64/64/1',
+  'name': '홍길동',
+  'birthday':'970418',
+  'gender':'남성',
+  'jab':'대학생'
+},{
+  'id':2,
+  'image':'https://placeimg.com/64/64/2',
+  'name': '토마토',
+  'birthday':'951009',
+  'gender':'남성',
+  'jab':'직장인'
+},{
+  'id':3,
+  'image':'https://placeimg.com/64/64/3',
+  'name': '우아에',
+  'birthday':'910617',
+  'gender':'여성',
+  'jab':'디자이너'
+}
+]
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"/>
-      <h2>Let's develop management system!</h2>
+    <div>
+      {
+        customers.map(c =>{
+          return(
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              jab={c.jab}
+            />
+          );
+        })
+      }
     </div>
   );
 }
