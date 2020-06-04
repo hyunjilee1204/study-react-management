d17d13bbf92f99852bec6ed398b2284be1e1525e
Jaeho Choi <jaeho@Jaehoui-MacBookPro.local>
2020-06-04 14:49:34 +0900

    add Databease Connection & CustomerAdd UI

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
@@ -5,6 +6,20 @@ const port = process.env.PORT||5000;
 
 app.use(bodyParser.json());//json type data
 app.use(bodyParser.urlencoded({extended: true}));
+
+// const data = fs.readFileSync('./database.json');
+// const conf = JSON.parse(data);
+// const mysql = require('mysql');
+
+// const connection = mysql.createConnection({
+//   host:conf.host,
+//   user:conf.user,
+//   password:conf.password,
+//   port:conf.port,
+//   database:conf.database
+// });
+
+// connection.connect();
 
 app.get('/api/customers', (req,res)=>{
     res.send([{
@@ -30,5 +45,14 @@ app.get('/api/customers', (req,res)=>{
         'jab':'디자이너'
       }]);
 });
+
+// app.get('/api/db/customers', (req,res) =>{
+//   connection.query(
+//     "SELECT * FROM CUSTOMER",
+//     (err,rows, fields) =>{
+//       res.send(rows);
+//     }
+//   )
+// });
 
 app.listen(port, ()=> console.log(`Listening on port ${port}`));
